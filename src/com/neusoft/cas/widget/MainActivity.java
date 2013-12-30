7ecc6ec35167f8edfa4257dd4dde5fb5fa0ea141
ycj0808 <ycj0808@126.com>
2013-12-31 00:09:13 +0800

    Signed-off-by: ycj0808 <ycj0808@126.com>

--- src/com/neusoft/cas/widget/MainActivity.java
+++ src/com/neusoft/cas/widget/MainActivity.java
@@ -1,13 +1,19 @@
 package com.neusoft.cas.widget;
 
+import net.simonvt.menudrawer.MenuDrawer;
+
 import com.actionbarsherlock.view.Menu;
+
 import android.os.Bundle;
 
 public class MainActivity extends BaseActivity {
 
+	private MenuDrawer mDrawer;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		mDrawer=getmDrawer();
+		mDrawer.setContentView(R.layout.layout_main);
 	}
 
 	@Override
